ab17e808f2871cac091eb575d9e874283215007b
Yudiz-Tirthana <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-10-17 11:33:14 +0530

    Add files via upload

---
+++ Program2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to get fibonacci :");
+            int number = int.Parse(Console.ReadLine());
+            int a = 0;
+            int b = 1;
+
+            Console.Write($"{a} {b} ");
+
+            for(int i=1 ; i<= number; i++){
+                int c = a + b;
+                Console.Write($"{c} ");
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
