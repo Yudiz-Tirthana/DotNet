bea04c11a353ef61d0a477e44a6af155bb59a825
Yudiz-Tirthana <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-10-17 11:34:40 +0530

    Add files via upload

---
+++ Program3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program3
+{
+    class Program3
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number for the factorial :");
+            int number = int.Parse(Console.ReadLine());
+
+            int count=0;
+
+            for(int i=1 ; i<= number; i++){
+                if(number % i == 0){
+                    count++;
+                }
+            }
+            if(count == 2){
+                Console.WriteLine($"{number} is the prime number");
+            }
+            else{
+                Console.WriteLine($"{number} is not the prime number");
+            }
+        }
+    }
+}
