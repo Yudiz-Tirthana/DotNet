999c2dc6574f02f8982b20870fdadc4afb411d74
Yudiz-Tirthana <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-10-17 11:37:32 +0530

    Add files via upload

---
+++ Program6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum=0, temp;
+            Console.WriteLine("Enter the number: ");
+            n = int.Parse(Console.ReadLine());
+
+            temp = n;
+
+            while(n>0){
+                r = n%10;
+                sum = sum +(r*r*r);
+                n = n/10;
+            }
+            if(temp == sum){
+                Console.WriteLine("The given number is Armstrong number.");
+            }
+            else{
+                Console.WriteLine("The given number is not Armstrong number.");
+            }
+        }
+    }
+}
