008305cafa83702ded573e9c42ae43e63873627b
Yudiz-Tirthana <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-10-17 11:35:48 +0530

    Add files via upload

---
+++ Program4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number to check whether the number is palindrome or not :");
+            int number = int.Parse(Console.ReadLine());
+
+            int reminder, sum = 0;
+            int temp = number;
+
+            while(number > 0){
+                reminder=number%10;
+                sum = (sum*10) + reminder;
+                number = number/10;
+            }
+            if(temp == sum){
+                Console.WriteLine($"The entered number is Palindrome");
+            }
+            else{
+                Console.WriteLine($"The entered number is not Palindrome");
+            }
+
+           
+        }
+    }
+}
---
+++ Program5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program6
+    {
+        static void Main(string[] args)
+        {
+            string Str, revStr = "";
+            Console.WriteLine("Enter the string to check whether it is a palindrome :");
+            Str = Console.ReadLine();
+
+            for(int i=Str.Length-1; i >= 0; i--){
+                revStr += Str[i];
+            }
+            if(Str == revStr){
+                Console.WriteLine($"The entered String {Str} is Palindrom");
+            }
+            else
+            {
+                Console.WriteLine($"The entered String {Str} is not Palindrom");
+            }
+        }
+    }
+}
