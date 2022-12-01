71b19af4b092867761f9b40d3b12ce678c937d85
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-01 17:45:33 +0530

    Add files via upload

---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] numbers = new int[] { 1, 2, 3, 4, 5, 3, 5, 4, 2, 1, 3, 4, 2, 5, 6, 3, 3, 2, 4, 5, 6, 7, 4, 8, 8, 8, 7, 6, 2, 4, 2, 1, 1, 2 };
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 1, 2, 2 };
+
+            var result = from num in numbers
+                         group num by num into y
+                         select y;
+
+            foreach(var res in result)
+            {
+                Console.WriteLine("Number : " + res.Key + " Count " + res.Count() + " of the number");
+            }
+        }
+    }
+}
