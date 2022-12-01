f2d505e78620cd24ac659a837df7630b4cf09ab8
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-01 17:23:59 +0530

    Add files via upload

---
+++ Question2/Question2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 5,6,8,10,12,15,9,13,19,23};
+
+            var result = from n in numbers
+                         where n % 2 == 0
+                         select n;
+
+            foreach (var num in result)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
---
+++ Question3/Question3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1,2,3,4,5,6,7,8,9,10,11,12};
+
+            var result = from num in numbers
+                         let sqrt = num * num
+                         select new { num, sqrt };
+
+            foreach(var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
