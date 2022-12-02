a420009a46f69985b610e3aa6b3480f24a486654
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-02 09:44:20 +0530

    Add files via upload

---
+++ Question10/Question10/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question10
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {   
+            List<int> templist = new List<int>();
+            Console.WriteLine("Input the number of members on the List ");
+            var number =Convert.ToInt32(Console.ReadLine());
+            
+            for(int i = 0; i<number ; i++ )
+            {
+                Console.Write("Member {0} : ", i);
+                int memlist = Convert.ToInt32(Console.ReadLine());
+                templist.Add(memlist);
+            }
+
+            Console.WriteLine("Input the value above you want to display the members of the list :");
+            int check_number = Convert.ToInt32(Console.ReadLine());
+
+            List<int> FilterList = templist.FindAll(x => x > check_number ? true : false);
+            Console.WriteLine("The numbers greater than {0} are : ",check_number);
+
+            foreach(var temp_num in FilterList)
+            {
+                Console.WriteLine(temp_num);
+            }
+            Console.ReadLine();
+        }
+    }
+}
