71c97df69b1e7819d1409237c1fdd803d575e575
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-05 11:36:59 +0530

    Add files via upload

---
+++ Qusetion5/Qusetion5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Qusetion5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "apple";
+
+            var result = from ch in str
+                         group ch by ch into y
+                         select y;
+
+            foreach(var res in result)
+            {
+                Console.WriteLine("Character : " + res.Key + " Appered " + res.Count() + " times in the string");
+            }
+        }
+    }
+}
