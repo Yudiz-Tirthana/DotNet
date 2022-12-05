43c895c56086df824446e12f8f5e9c78ea94bea0
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-05 13:44:05 +0530

    Add files via upload

---
+++ Question7/Question7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[] { 1, 2, 3, 4, 5, 5, 4, 3, 2, 1, 1, 2, 2 };
+
+            var result = from num in numbers
+                         group num by num into y
+                         select y;
+
+            foreach (var res in result)
+            {
+                Console.WriteLine("Number : " + res.Key + " Frequency " + res.Count() + " Multiplication :" + (res.Key * res.Count()));
+            }
+        }
+    }
+}
