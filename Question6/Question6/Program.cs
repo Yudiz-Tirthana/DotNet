fded6bf56111b49c09e5c7616e6e3f41c0a82981
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-05 11:39:26 +0530

    Add files via upload

---
+++ Question6/Question6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Days = new string[]{ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            Console.WriteLine("Days in a Week :");
+
+            var result = from Day in Days
+                         select Day;
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
