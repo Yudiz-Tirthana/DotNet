0116ec1bc9be1feb783c9dee0b74ae012bc08c28
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-05 14:17:27 +0530

    Add files via upload

---
+++ Question8/Question8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Cities = new string[] { "rome", "london", "nairobi", "california", "zurich", "new Delhi", "amsterdam", "abu dhabi", "paris" };
+
+            var result = from city in Cities
+                         where city.StartsWith("a")
+                         where city.EndsWith("m")
+                         select city;
+
+            foreach(var res in result)
+            {
+                Console.WriteLine("Cities are : " + res);
+            }
+        }
+    }
+}
---
+++ Question9/Question9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[] {1,12,32,234,341,54,34,542,2465,356,635,67,25,245,245,256,635,76,875,75,87,78,89,90,900,123};
+
+            var result = from num in Numbers
+                         where num > 80
+                         select num;
+
+            Console.WriteLine("Numbers greater than 80 are  ");
+            foreach (var res in result)
+            {
+                Console.WriteLine(res);
+            }
+        }
+    }
+}
