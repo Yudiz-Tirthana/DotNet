9dfa09be7e048d45ba9edd9979057d7089d2b738
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-06 10:05:03 +0530

    Add files via upload

---
+++ Question12/Question12/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find the UpperCase words in a string : ");
+
+            string strNew;
+
+            Console.WriteLine("Input the String");
+            strNew = Console.ReadLine();
+
+            var UpperCaseWord = WordFilt(strNew);
+            Console.WriteLine("The UPPERCASE words are : ");
+            foreach (string strReturn in UpperCaseWord)
+            {
+                Console.WriteLine(strReturn);
+            }
+            Console.ReadLine();
+        }
+        static IEnumerable<string> WordFilt(string mystr)
+        {      
+            var upWord = mystr.Split(' ').Where(x => String.Equals(x, x.ToUpper(), StringComparison.Ordinal));
+
+            return upWord;
+        }
+    }
+}
