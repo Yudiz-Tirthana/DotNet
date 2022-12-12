c7a75bc3273c0fbfe6b5393e648ac8d448ba2c66
Tirthana Kansagara <113357872+Yudiz-Tirthana@users.noreply.github.com>
2022-12-12 13:43:43 +0530

    Add files via upload

---
+++ Question28/Question28/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = { "Rome", "London", "Nairobi", "Califonia", "Zurich", "New Delhi","Amsterdam","Abu Dhabi","Paris" };
+            Console.WriteLine("The list of the citeis according to the length and then by the name in ascending order : ");
+            IEnumerable<string> cityOrder = cities.OrderBy(str => str.Length).ThenBy(str => str);
+            foreach (string city in cityOrder)
+                Console.WriteLine(city);
+            Console.ReadLine();
+        }
+    }
+}
